39b3abd4a9f558def3c617511136f51dd042dfed
umeda Davlatova <umeda.davlatova@gmail.com>
2020-11-12 22:39:46 -0500

    working on last project

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello Git last day");
+
+        System.out.println("B21 is excited ");
+
+
     }
 }
